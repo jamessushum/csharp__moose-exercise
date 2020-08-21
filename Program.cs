29f8582f2253b6b3290c422271bd4792d7d539a2
James Su-Shum <sushumjames@gmail.com>
2020-08-21 13:33:51 -0400

    Added MooseAsks method taking a question as parameter

--- Program.cs
+++ Program.cs
@@ -13,6 +13,31 @@ namespace EnthusiasticMoose
             // Let the moose speak!
             MooseSays("H I, I'M  E N T H U S I A S T I C !");
             MooseSays("I really am enthusiastic");
+
+            // Ask a question
+            bool isTrue = MooseAsks("Is Canada real?");
+            Console.WriteLine(isTrue);
+        }
+
+        static bool MooseAsks(string question)
+        {
+            Console.Write($"{question} (y/n): ");
+            string answer = Console.ReadLine().ToLower();
+
+            while (answer != "y" && answer != "n")
+            {
+                Console.Write($"{question} (y/n): ");
+                answer = Console.ReadLine().ToLower();
+            }
+
+            if (answer == "y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         static void MooseSays(string message)
